fb51fef86d2d88305ce1b66dd37941503388b030
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2018-02-20 22:27:28 -0500

    got svg in hero/header container

--- src/layouts/MainHeader/MainHeader.jsx
+++ src/layouts/MainHeader/MainHeader.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import classNames from "classnames";
-import "./MainHeader.css";
+import React from 'react';
+import classNames from 'classnames';
+import './MainHeader.css';
 
 class MainHeader extends React.Component {
   render() {
     const { children, cover } = this.props;
 
-    const classes = classNames("main-header", this.props.className, {
-      "no-cover": !cover
+    const classes = classNames('main-header', this.props.className, {
+      'no-cover': !cover,
     });
 
     const getStyle = () => {
@@ -18,7 +18,24 @@ class MainHeader extends React.Component {
     };
 
     return (
-      <header className={classes} style={getStyle()}>
+      <header className={classes}>
+        <div className="polygon">
+          {/*    preserveAspectRatio: makes sure the triangle is scalable
+      viewbox: points stretches from the parent
+  */}
+          <svg preserveAspectRatio="none" viewBox="0 0 100 100">
+            <polygon points="100,0,100,100,0,100" opacity="1" transform="scale(-2,2) translate(-100,0)" />
+          </svg>
+        </div>
+        <div className="polygon-2 polygon">
+          {/*    preserveAspectRatio: makes sure the triangle is scalable
+      viewbox: points stretches from the parent
+  */}
+          <svg preserveAspectRatio="none" viewBox="0 0 100 100">
+            <polygon points="100,100 100,0 0,100" opacity="1" transform="scale(2,-2) translate(0,-100)" />
+          </svg>
+        </div>
+
         {children}
       </header>
     );
