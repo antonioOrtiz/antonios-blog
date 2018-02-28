e242cb1b415cbd274adfb573d4dabaae3939e026
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2018-02-28 12:48:11 -0500

    not sure what was updated

--- src/layouts/MainNav/MainNav.jsx
+++ src/layouts/MainNav/MainNav.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import classNames from "classnames";
-import "./MainNav.css";
+import React from 'react';
+import classNames from 'classnames';
+import './MainNav.css';
 
 class MainNav extends React.Component {
   render() {
     const { children, overlay } = this.props;
 
-    const classes = classNames("main-nav", this.props.className, {
-      overlay
+    const classes = classNames('main-nav', this.props.className, {
+      overlay,
     });
 
     return <nav className={classes}>{children}</nav>;
