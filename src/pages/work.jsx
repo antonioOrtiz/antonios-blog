5b4f705784c86fb4f6515b0ab4a8916f5d51b4bd
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2018-02-28 12:36:22 -0500

    change of className from about to work

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -6,7 +6,7 @@ import config from '../../data/SiteConfig';
 class WorkPage extends Component {
   render() {
     return (
-      <div className="about-container">
+      <div className="work-container">
         <Helmet title={`Work | ${config.siteTitle}`} />
         <Work />
       </div>
