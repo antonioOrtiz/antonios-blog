7e70a4442ddc755b84d423516486fff76deabb3d
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2018-03-05 12:55:51 -0500

    resume button added to page

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { Link } from 'react-scroll';
-import { navigateTo } from 'gatsby-link';
+// import { navigateTo } from 'gatsby-link';
 
-import { Container, Header, Item } from 'semantic-ui-react';
+import { Container, Header, Item, Grid, Button } from 'semantic-ui-react';
 import SEO from '../SEO/SEO';
 import config from '../../../data/SiteConfig';
 import Drawer from '../../layouts/Drawer/Drawer';
@@ -17,6 +17,8 @@ import MenuButton from '../MenuButton/MenuButton';
 import PageTitle from '../PageTitle/PageTitle';
 import PageDescription from '../PageDescription/PageDescription';
 import SocialMediaIcons from '../SocialMediaIcons/SocialMediaIcons';
+
+import Resume from '../../../docs/Antonio-Pavicevac-Ortiz.pdf';
 import './Work.css';
 
 class Work extends React.Component {
@@ -57,8 +59,15 @@ class Work extends React.Component {
     const { nodes } = this.props;
     const style = {
       h1: {
-        margin: '1em 0 1em',
+        margin: '.5em 0 .5em',
         fontSize: '55px',
+      },
+      button: {
+        backgroundColor: '#D6002A',
+        color: '#fff',
+        marginTop: '15px',
+        padding: '15px 50px 15px',
+        fontSize: '.75em',
       },
     };
 
@@ -90,13 +99,18 @@ class Work extends React.Component {
                 <span className="hidden">Scroll Down</span>
               </Link>
             </MainHeader>
-            <Header as="h1" content="Projects" style={style.h1} textAlign="center" />{' '}
             <Container>
-              <MenuButton />
-
+              <Header as="h1" content="My background " style={style.h1} textAlign="left" />{' '}
+              <Grid.Column width={16}>
+                {/* prettier-ignore */}
+                <Item.Description>{`Having completed my course study to become a Software Developer at FullStack Academy! \\o/ I really like every part of the stack, but  love the relationship between UI and an API. I'd like to find a position at a company where the people are focused on learning and teaching each other while solving problems!`}</Item.Description>
+                <Button as="a" href={Resume} target="_blank" style={style.button}>
+                  Download resume
+                </Button>
+              </Grid.Column>
               <Item.Group divided>
+                <Header as="h1" content="Projects " style={style.h1} textAlign="left" />{' '}
                 <Item>
-                  <Item.Image src="/assets/images/wireframe/image.png" />
                   <Item.Content>
                     <Item.Header href="https://github.com/BeanieBoop" target="_blank" as="a">
                       Beanie Boop
@@ -108,7 +122,6 @@ class Work extends React.Component {
                   </Item.Content>
                 </Item>
                 <Item>
-                  <Item.Image src="/assets/images/wireframe/image.png" />
                   <Item.Content>
                     <Item.Header href="https://github.com/capstone-fitsquare/fitsquare" target="_blank" as="a">
                       Fit Square
@@ -118,6 +131,19 @@ class Work extends React.Component {
                     </Item.Meta>
                     <Item.Description>
                       A food planning app that helps you build a 5-day meal plan based on your current fitness goal.
+                    </Item.Description>
+                  </Item.Content>
+                </Item>
+                <Item>
+                  <Item.Content>
+                    <Item.Header href="https://ants-omr-pinteresting.herokuapp.com/" target="_blank" as="a">
+                      Pinterest Clone
+                    </Item.Header>
+                    <Item.Meta>
+                      <span>May 2015</span>
+                    </Item.Meta>
+                    <Item.Description>
+                      {`Essentially my first app, I created while going through the "One Month Rails" course!`}
                     </Item.Description>
                   </Item.Content>
                 </Item>
